0fb4030a06691640fea66aa0528726f17dfd159e
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-04-01 07:41:26 -0400

    Add lavador.js to manage personal and stock image folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import Lavador from './Lavador'
+import imgPerfil from './images/imgperfil.jpg'
+
 function App() {
+
+    return <>
+    <Lavador />
+    {/* <h1>Hi there</h1>
+    <img src={imgPerfil} className='perfil' alt='imagend de perfil'/>
+     */}
+    </>
     
-    return <h1>Hi there</h1>
+   
+
+    
 }
 
 export default App;
---
+++ src/Lavador.js
@@ -0,0 +1,14 @@
+import imgPerfil from './images/imgperfil.jpg'
+
+function Lavador() {
+    return <div>
+       
+    <h1>Name</h1>
+    <h2>Total car washed:</h2>
+    <img src={imgPerfil} className='perfil' alt='imagend de perfil'/>
+    </div>
+}
+
+export default Lavador;
+
+//Foto y nombre del lavador
